3901df080e513fd838a75d74d66a57e0d426e78c
MaxDeale <maxericdeale@gmail.com>
2020-04-06 12:54:54 +0200

    deploy

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 // connect to DB
 connectDB();
@@ -9,13 +10,15 @@ connectDB();
 // Init middleware
 app.use(
   express.json({
-    extended: false
+    extended: false,
   })
 );
 
+app.use(cors());
+
 app.get("/", (req, res) =>
   res.json({
-    msg: "Welcome to the Pokedex API"
+    msg: "Welcome to the Pokedex API",
   })
 );
 
@@ -26,13 +29,13 @@ app.use("/api/auth", require("./routes/auth"));
 
 //serve static assets in production
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.port || 5000;
 
